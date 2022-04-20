d1d5afaa56ff6fc1cc7d81c9e11455b9c22261dd
Archanciel <jp.schnyder@gmail.com>
2022-04-20 13:31:44 +0200

    Update date_time_explore.dart

--- date_time_explore/date_time_explore.dart
+++ date_time_explore/date_time_explore.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:intl/intl.dart';
 
-// adding method HHmm which returns the Duration formatted as HH:mm 
+// adding method HHmm which returns the Duration formatted as HH:mm
 extension on Duration {
   static final NumberFormat numberFormat = NumberFormat('00');
 
@@ -15,38 +15,58 @@ extension on Duration {
 }
 
 void main() {
-  const Duration onBedDuration = Duration(hours: 13, minutes: 15);
-  const Duration wakeDuration = Duration(hours: 24);
-  final Duration duration = onBedDuration + wakeDuration;
-  final String dutationStr = duration.HHmm();
-
-  Duration dur = Duration(minutes: 58);
-
-  print('validating that Duration.inHours ignores the duration minutes');
-
-  for (int i = 0; i < 3; i++) {
-    dur = dur + Duration(minutes: 1);
-    print(dur.HHmm());
-  }
-
-  print('');
-
   final String localName = Platform.localeName;
   initializeDateFormatting(localName);
-  final DateFormat localDdateFormat = DateFormat('dd-MM-yyyy HH:mm', localName);
+  final DateFormat localDateFormat = DateFormat('dd-MM-yyyy HH:mm', localName);
   final DateFormat localDateFormatDayName =
       DateFormat("EEEE dd-MM-yyyy HH:mm", localName);
 
-  DateTime previousDate = localDdateFormat.parse('19-04-2022 15:30');
+  const Duration onBedDurationUserInput = Duration(hours: 12, minutes: 30);
+  final String onBedDurationStr = onBedDurationUserInput.HHmm();
+  const Duration wakeDurationUserInput = Duration(hours: 18);
+  final String wakeDurationStr = wakeDurationUserInput.HHmm();
+  final Duration circadianDuration = onBedDurationUserInput + wakeDurationUserInput;
+  final DateTime todayWakeUpDateUserInput = localDateFormat.parse('16-04-2022 21:00');
+  final String todayWakeUpDateStr =
+      localDateFormatDayName.format(todayWakeUpDateUserInput);
+
+  final DateTime planifiedWakeUpTimeUserInput = localDateFormat.parse('19-04-2022 15:30');
+  DateTime previousDate = planifiedWakeUpTimeUserInput;
   String previousDateStr = localDateFormatDayName.format(previousDate);
-  String nextDateStr = '';
+  String whenGoToBedStr = '';
+  String whenWakeUpStr = '';
+  DateTime beforeLimit;
+
   print('planified wake up time: $previousDateStr');
 
-  for (int i = 0; i < 3; i++) {
-    final DateTime nextDate = previousDate.subtract(duration);
-    nextDateStr = localDateFormatDayName.format(nextDate);
-    print('$previousDateStr - $dutationStr = $nextDateStr');
-    previousDate = nextDate;
+  while (true) {
+    final DateTime whenGoToBed = previousDate.subtract(onBedDurationUserInput);
+//    beforeLimit = previousDate.subtract(circadianDuration);
+
+    if (whenGoToBed.isBefore(todayWakeUpDateUserInput)) {
+      break;
+    }
+
+    previousDate = whenGoToBed;
+
+    whenGoToBedStr = localDateFormatDayName.format(whenGoToBed);
+    print(
+        'go to bed at: $whenGoToBedStr ($previousDateStr - $onBedDurationStr)');
+    final DateTime whenWakeUp = previousDate.subtract(wakeDurationUserInput);
+    beforeLimit = previousDate.subtract(circadianDuration);
+
+    if (beforeLimit.isBefore(todayWakeUpDateUserInput)) {
+      previousDate = whenGoToBed;
+      break;
+    }
+
+    previousDate = whenWakeUp;
+    whenWakeUpStr = localDateFormatDayName.format(whenWakeUp);
+    print('wake up at: $whenWakeUpStr ($whenGoToBedStr - $wakeDurationStr)');
     previousDateStr = localDateFormatDayName.format(previousDate);
   }
+
+  final Duration diffBeforeNextGoToBed = previousDate.difference(todayWakeUpDateUserInput);
+  print('today wake up date: $todayWakeUpDateStr ${diffBeforeNextGoToBed.HHmm()} before next go to bed');
+  print('circadian duration: ${circadianDuration.HHmm()}');
 }
