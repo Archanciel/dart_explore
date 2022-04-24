2769ce65c84092ab5e04e700ecbc308cc003bd95
Archanciel <jp.schnyder@gmail.com>
2022-04-24 07:11:36 +0200

    exploring further

---
+++ date_time_explore/dart_tool/duration_explore.dart
@@ -0,0 +1,41 @@
+import 'package:intl/intl.dart';
+
+// adding method HHmm which returns the Duration formatted as HH:mm
+extension on Duration {
+  // When you use your extension in another file,
+//                          you must give it a name !
+  static final NumberFormat numberFormatTwoInt = NumberFormat('00');
+
+  /// returns the Duration formatted as HH:mm
+  String HHmm() {
+    int durationMinute = this.inMinutes.remainder(60);
+    String minusStr = '';
+
+    if (this.inMinutes < 0) {
+      if (this.inHours == 0) {
+        minusStr = '-';
+      }
+      durationMinute = -durationMinute;
+    }
+
+    return "$minusStr${this.inHours}:${numberFormatTwoInt.format(durationMinute)}";
+  }
+}
+
+void main() {
+  Duration dur = Duration(minutes: 58);
+
+  for (int i = 0; i < 3; i++) {
+    dur = dur + Duration(minutes: 1);
+    print(dur.HHmm());
+  }
+
+  print((dur - Duration(hours: 1, minutes: 2)).HHmm());
+  print(Duration(minutes: -5).HHmm());
+  final Duration durationHoursOneDigit = Duration(hours: -1, minutes: -5);
+  print(durationHoursOneDigit.HHmm());
+  print(durationHoursOneDigit);
+  final Duration durationHoursTwoDigits = Duration(hours: -12, minutes: -5);
+  print(durationHoursTwoDigits.HHmm());
+  print(durationHoursTwoDigits);
+}
