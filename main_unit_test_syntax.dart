8a09f83cd7b77859335a094509c5078139d83086
Archanciel <jp.schnyder@gmail.com>
2022-06-10 02:05:44 +0200

    ok

---
+++ main_unit_test_syntax.dart
@@ -0,0 +1,33 @@
+void main() {
+  group("some_Group_Name 1", () {
+    test("Description of the test 1", () {
+      String actualValue = '';
+      String matchingValue = '';
+      expect(actualValue, matchingValue);
+    });
+    test("Description of the test 2", () {
+      String actualValue = '';
+      String matchingValue = '';
+      expect(actualValue, matchingValue);
+    });
+  });
+
+  group("some_Group_Name 2", () {
+    test("Description of the test 1", () {
+      String actualValue = '';
+      String matchingValue = '';
+      expect(actualValue, matchingValue);
+    });
+    test("Description of the test 2", () {
+      String actualValue = '';
+      String matchingValue = '';
+      expect(actualValue, matchingValue);
+    });
+  });
+
+  test("Description of the test 1", () {
+    String actualValue = '';
+    String matchingValue = '';
+    expect(actualValue, matchingValue);
+  });
+}
