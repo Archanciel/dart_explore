e1f049c480032ac8dd85276c577d2a0d468474b6
Archanciel <jp.schnyder@gmail.com>
2022-06-10 08:40:07 +0200

    Create map_operator_applied_to_map_collection.dart

---
+++ map_operator_applied_to_map_collection.dart
@@ -0,0 +1,15 @@
+void main() {
+  Map<String, int> srcMap = {"a": 1, "b": 2};
+  print(srcMap); // {a: 1, b: 2}
+
+  Map<String, int> targetMap = srcMap.map((k, v) {
+    k = k + k;
+    v = v * 10;
+
+    return MapEntry(k, v);
+  });
+
+  print(targetMap); // {aa: 10, bb: 20}
+
+  print(srcMap); // {a: 1, b: 2}
+}
