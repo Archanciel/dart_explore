af8089d8b885e73b569dad1af5d8b315075bd188
Archanciel <jp.schnyder@gmail.com>
2024-02-29 21:35:55 +0100

    Create main_enum_to_list.dart

---
+++ main_enum_to_list.dart
@@ -0,0 +1,17 @@
+enum SortingOption {
+  audioDownloadDateTime,
+  videoUploadDate,
+  validAudioTitle,
+  audioEnclosingPlaylistTitle,
+  audioDuration,
+  audioFileSize,
+  audioMusicQuality,
+  audioDownloadSpeed,
+  audioDownloadDuration,
+  videoUrl, // useful to detect audio duplicates
+}
+
+enum dataKey {currSleep, currWakeUp, newDateTime}
+
+void main() {
+}  
